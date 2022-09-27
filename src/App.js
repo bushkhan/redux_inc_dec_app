a4f547ef0f532be0dad670c7bae6cbfe09bb4446
Bushra Khan <bushrakhan2212002@gmail.com>
2022-09-27 21:25:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import "./App.css";
 
 const App = () => {
+
+
+
   return (
-    <div className='font-family:foont-mono font-style: italic text-2xl text-center'>Hello</div>
+    <>
+      
+
+      <div class="container">
+  
+      <h1>Increment/Decrement counter</h1>
+      <h4>using React and Redux</h4>
+      
+      <div class="quantity">
+        <a class="quantity__minus" title="Decrement"><span>-</span></a>
+        <input name="quantity" type="text" class="quantity__input" value="0" />
+        <a class="quantity__plus" title="Increment"><span>+</span></a>
+      </div>
+  
+          </div>
+     
+    </>
   )
 }
 
